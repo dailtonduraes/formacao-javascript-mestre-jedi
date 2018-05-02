b21a186fa2f118258b9f8eb9662aa03419f04e5e
dailtonduraes <dailtonsd10@gmail.com>
2018-05-02 16:57:39 -0300

    commit primeira missão

--- missao-estelar/primeira-missao/dduraes/js/funcoes.js
+++
@@ -1,9 +0,0 @@
-var missao = "Primeira missão estelar";
-
-function eventLoad(){
-    console.log('Carregou a pagina');
-}
-
-function eventClick(){
-    console.log('Clicou no botão');
-}
---
+++ missao-estelar/primeira-missao/dduraes/js/js.js
@@ -0,0 +1,9 @@
+var missao = "Missão Estelar JS em execução!!!";
+
+function eventoOnload(){
+    console.log('Carregou a pagina');
+}
+
+function eventoOnclick(){
+    console.log('Missão Estelar JS realizada com sucesso!!!');
+}
